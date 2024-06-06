b977c1b89b8158fc7b1ee14a0fbdc6c41c311c1b
sum_catnip <catnip@catnip.fyi>
2024-06-06 18:31:26 +0000

    more maff

--- turtles.py
+++ turtles.py
@@ -51,7 +51,23 @@ def factors(n: int) -> List[int]:
 def gcf(a: int, b: int) -> int:
     return max(set(factors(a)).intersection(factors(b)))
 
-shape('turtle')
+def avg(*args: float) -> float:
+    return sum(args) / len(args)
+
+def find_sqrt(num: int, low: float, high: float) -> float:
+    for _ in range(100):
+        if (h := avg(low, high)) ** 2 > num: high = h
+        elif (l := avg(low, high)) ** 2 < num: low = l
+        else: break
+
+    return avg(low, high)
+
+# solve first degree equasion for x
+# ax + b = cx + d
+def solve(a: float, b: float, c: float, d: float) -> float:
+    return (d - b) / (a - c)
+
+#shape('turtle')
 #square(100)
 #squircle(100)
 #triangle(100)
@@ -62,4 +78,11 @@ shape('turtle')
 #print(seqsum(100))
 print(factors(120))
 print(gcf(150, 138))
+sqrt = find_sqrt(50000, 1, 500)
+print(sqrt)
+print(sqrt ** 2)
+
+print(solve(2, 5, 0, 13))
+print(solve(12, 18, -34, 67))
+print(solve(1/2, 2/3, 1/5, 7/8))
 #done()
